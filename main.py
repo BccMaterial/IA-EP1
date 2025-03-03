069c07de6b0a6e74503c4cf64efac042284e8657
viniciusegydio <egydiovinicius022@gmail.com>
2025-03-03 17:24:43 -0300

    removed useless comment

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
 def config_inicial(arquivo_configuracao): #Recebe um arquivo txt para configurar os dados iniciais
-    # O arquivo txt deve seguir o seguinte padrão:
-    # 5: Número de posições
-    # 3: Altura máxima das pilhas(sempre será 3)
-    # 30 0 0: Posição 0: caixa de 30kg no chão
-    # 20 0 0: Posição 1: caixa de 20kg no chão
-    # B: Posição 2 braço mecânico
-    # 10 5 0: Posição 3: caixas de 10kg e 5kg empilhadas
-    # 7 2 1: Posição 4: caixas de 7kg, 2kg e 1kg empilhadas
 
     with open(arquivo_configuracao, "r") as arquivo: #Utilizamos open para que o arquivo seja fechado automaticamente #"r" significa modo de leitura
         linhasArquivo = arquivo.read().strip().split("\n") #.strip() é utilizado para remover os espaços em branco e .split() é usado para transformar cada palavra em um elemento de uma lista
