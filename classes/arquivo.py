84e3644f3d8715c8fc391afbdb49a8a9168bb7df
thiagopls1 <thiagopls1@hotmail.com>
2025-03-09 01:05:19 -0300

    File write setup

--- classes/arquivo.py
+++ classes/arquivo.py
@@ -3,3 +3,7 @@ def ler(caminho):
     with open(caminho, "r") as arquivo:
         str_arquivo = arquivo.read()
     return str_arquivo
+
+def escrever(caminho, string):
+    with open(caminho, "w+") as arquivo:
+        arquivo.write(string)
