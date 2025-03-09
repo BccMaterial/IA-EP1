25e1ccebb384ce979efc58986a5c984a44c4ebce
viniciusegydio <egydiovinicius022@gmail.com>
2025-03-09 02:26:32 -0300

    created tupla_to_string() and escrever()

--- classes/arquivo.py
+++ classes/arquivo.py
@@ -1,9 +1,17 @@
+def tupla_to_string(tuplas):
+    return "\n".join(" ".join(map(str, sub_tupla)) for sub_tupla in tuplas)
+
+
 def ler(caminho):
     str_arquivo = ""
     with open(caminho, "r") as arquivo:
         str_arquivo = arquivo.read()
     return str_arquivo
 
-def escrever(caminho, string):
-    with open(caminho, "w+") as arquivo:
-        arquivo.write(string)
+def escrever(caminho, arquivo):
+    with open(arquivo, "w+") as arquivo:
+        for i, pilhas in enumerate(caminho):
+            arquivo.write(f"Passo {i} \n")
+            for pilha in pilhas:
+                arquivo.write(" ".join(map(str, pilha)) + "\n")
+            arquivo.write("\n")
