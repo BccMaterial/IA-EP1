1ff0e41dd38e6d79fb35e5a2562bb09e3fe528cd
thiagopls1 <thiagopls1@hotmail.com>
2025-03-09 21:56:52 -0300

    Add input to `alinhamento`

--- main.py
+++ main.py
@@ -11,29 +11,34 @@ if caminho is None or caminho == "":
 elif os.path.isfile(caminho):
     print("Caminho não encontrado. Sorteando pilhas...")
 
-alinhamento = input("Insira para qual lado a pilha deve ser organizada (E - Esq. D - Dir.): ")
+alinhamento = input("Insira para qual lado a pilha deve ser organizada (E - Esq. d - Dir.): ")
 
-problema = BracoMecanico(caminho, alinhamento)
+if alinhamento is None or alinhamento == "":
+    alinhamento = "E"
+
+problema = BracoMecanico(caminho, alinhamento.upper())
 problema_a_estrela = deepcopy(problema)
 print("Solução por A*")
 print("-----------------------------------------------------------------------")
 print(f"Estado Inicial: {problema_a_estrela.to_hashable(problema_a_estrela.bases_caixas)}")
-print()
 
 resultado_a_estrela = algoritmos.a_estrela(problema_a_estrela)
 if resultado_a_estrela[1] is not None:
     print(f"Solução encontrada: {resultado_a_estrela[1]}")
     print(f"Número de iterações: {resultado_a_estrela[0]}")
+    # Gera o arquivo txt do resultado com a_estrela:
+    no = resultado_a_estrela[1]
+    movimentos_a_estrela = algoritmos.no_caminho_aresta(no)
+    no_estado_a_estrela = arquivo.tupla_to_string(no.estado)
+    caminho_arquivo = "./output/a_estrela_sol.txt"
+    arquivo.escrever(caminho_arquivo, movimentos_a_estrela, no_estado_a_estrela)
+    print(f"Caminho e estado final armazenado em: {caminho_arquivo}")
 else:
     print(f"Não foi encontrada nenhuma solução (Número de iterações: {resultado_a_estrela[0]})")
 
 print()
 
-#Gera o arquivo txt do resultado com a_estrela:
-no_a_estrela = resultado_a_estrela[1]
-movimentos_a_estrela = algoritmos.no_caminho_aresta(no_a_estrela)
-no_estado_a_estrela = arquivo.tupla_to_string(no_a_estrela.estado)
-arquivo.escrever("./output/a_estrela_sol.txt", movimentos_a_estrela, no_estado_a_estrela)
+
 
 #Solução com dijkstra:
 problema_dijkstra = deepcopy(problema)
@@ -44,16 +49,19 @@ resultado_dijkstra = algoritmos.dijkstra(problema_dijkstra)
 if resultado_dijkstra[1] is not None:
     print(f"Solução encontrada: {resultado_dijkstra[1]}")
     print(f"Número de iterações: {resultado_dijkstra[0]}")
+    #Gera o arquivo txt do resultado com dijkstra:
+    no_dijkstra = resultado_dijkstra[1]
+    movimentos_dijkstra = algoritmos.no_caminho_aresta(no_dijkstra)
+    no_estado_dijkstra = arquivo.tupla_to_string(no_dijkstra.estado)
+    caminho_arquivo = "./output/dijkstra_sol.txt"
+    arquivo.escrever(caminho_arquivo, movimentos_dijkstra, no_estado_dijkstra)
+    print(f"Caminho e estado final armazenado em: {caminho_arquivo}")
 else:
     print(f"Não foi encontrada nenhuma solução (Número de iterações: {resultado_dijkstra[0]})")
 
 print()
 
-#Gera o arquivo txt do resultado com dijkstra:
-no_dijkstra = resultado_dijkstra[1]
-movimentos_dijkstra = algoritmos.no_caminho_aresta(no_dijkstra)
-no_estado_dijkstra = arquivo.tupla_to_string(no_dijkstra.estado)
-arquivo.escrever("./output/dijkstra_sol.txt", movimentos_dijkstra, no_estado_dijkstra)
+
 
 #Solução com greedy:
 problema_greedy = deepcopy(problema)
@@ -64,12 +72,15 @@ resultado_greedy = algoritmos.greedy(problema_greedy)
 if resultado_greedy[1] is not None:
     print(f"Solução encontrada: {resultado_greedy[1]}")
     print(f"Número de iterações: {resultado_greedy[0]}")
+    no_greedy = resultado_greedy[1]
+    movimentos_greedy = algoritmos.no_caminho_aresta(no_greedy)
+    no_estado_greedy = arquivo.tupla_to_string(no_greedy.estado)
+    caminho_arquivo = "./output/dijkstra_sol.txt"
+    arquivo.escrever("./output/dijkstra_sol.txt", movimentos_greedy, no_estado_greedy)
+    print(f"Caminho e estado final armazenado em: {caminho_arquivo}")
 else:
     print(f"Não foi encontrada nenhuma solução (Número de iterações: {resultado_greedy[0]})")
 
-no_greedy = resultado_greedy[1]
-movimentos_greedy = algoritmos.no_caminho_aresta(no_greedy)
-no_estado_greedy = arquivo.tupla_to_string(no_greedy.estado)
-arquivo.escrever("./output/greedy_sol.txt", movimentos_greedy, no_estado_greedy)
+
 
 
